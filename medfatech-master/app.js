dd1acf4209e734a8cad2a629e4e03c1b5a29e965
Ayo JB-CODE <ayotunde8@gmail.com>
2026-02-03 11:10:50 +0100

    Initial medaftech  website upload

---
+++ medfatech-master/app.js
@@ -0,0 +1,85 @@
+let header = document.querySelector("header");
+let menu = document.querySelector('.menu');
+let menuBtn = document.querySelector('.menu-btn');
+let closeBtn = document.querySelector('.close-btn');
+let slides = document.querySelectorAll('.slide');
+let btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+
+// Sticky Navbar
+window.addEventListener("scroll", function () {
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// Navbar responsiveness
+menuBtn.addEventListener("click", () => {
+    menu.classList.add('active');
+});
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove('active');
+});
+
+// Javascript for image slider manual navigation
+let manualNav = function (manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
+
+// Javascript for image slider autoplay navigation
+let repeat = function (activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    let repeater = () => {
+        setTimeout(function () {
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if (slides.length == i) {
+                i = 0;
+            }
+            if (i >= slides.length) {
+                return;
+            }
+            repeater();
+        }, 10000);
+    }
+    repeater();
+}
+repeat();
+
+function emailSend() {
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "ayotunde8@gmail.com",
+        Password: "0C7678E536C149898A189594AA1056252AE4",
+        To: 'jubril.babatunde.international@gmail.com',
+        From: "ayotunde8@gmail.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
